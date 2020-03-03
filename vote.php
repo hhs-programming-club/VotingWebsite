0e1075092246f2ace169d7985534349af3a1e7fd
kale <kalebkim1939@gmail.com>
2020-03-03 13:26:45 -0800

    idk too

--- vote.php
+++ vote.php
@@ -9,6 +9,10 @@
         <p>Navigation Bar</p>
         <div id="login">
             <p>Login goes here</p>
+            <form onsubmit="alert('success!')" method="post">
+            <input type="text" name="username"/>
+            <button type="submit" id="submit-button">Submit</button>
+        </form>
         </div>
     </div>
 
@@ -26,9 +30,6 @@
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flh3.googleusercontent.com%2FPgfJZomAD_4Q1BM9e9yxrvo6BXPX4BnYTCV23GIHGLGjFTEQS1UFR2-SF63r7llMAsq7t6Z-s2VBIH3hmr5zoA%3Ds400&f=1&nofb=1" alt="steve" height=100px>
             <p>I like being controlled by other people.</p>
         </div>
-        <form onsubmit="alert('success!')" method="post">
-            <input type="text" name="personName">Well this didn't work out</input>
-            <button type="submit" id="submit-button">Submit</button>
-        </form>
+
     </div>
 </body>
